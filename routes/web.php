051edaf92569bf332f6982710c6e0065470375e5
Anggita2302 <prithawidyaanggita@gmail.com>
2026-03-10 22:12:45 +0700

    Menambahkan template landing

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/', function () {
+    return view('landing');
+});
+
 Route::get('/login', function () {
     return view('login');
 });
